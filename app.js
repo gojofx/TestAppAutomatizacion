206e40b22b7d123000d6256089f6168f4aa247aa
Johan Londoño <johan.londono@eholding.com.co>
2024-03-06 01:32:42 -0500

    Feat Servidor ExpressJS Añadido

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Ruta de ejemplo
+app.get('/', (req, res) => {
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error('Error al leer el archivo:', err);
+          res.status(500).send('Error interno del servidor');
+          return;
+        }
+        // Parsea el contenido del archivo JSON
+        const jsonData = JSON.parse(data);
+        jsonData.tipoDato = "Quemado";
+        // Envía el contenido como respuesta
+        res.json(jsonData);
+      });
+});
+
+app.post('/', (req, res) => {
+  res.json(req.body);
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
